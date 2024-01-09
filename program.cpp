03872ccf3122664444f738f7424f0efddcb5fd9c
Ronald198 <ronaldlamani@gmail.com>
2024-01-09 08:45:51 +0100

    almost no changes

--- program.cpp
+++ program.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include <vector>
 #include "ref.hh"
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
 /*
 3
 3
@@ -79,31 +85,32 @@ void PrintMatrix(double **matrix, int nrOfRows, int nrOfColumns)
     {
         for (int j = 0; j < nrOfColumns; j++)
         {
-            std::cout << matrix[i][j] << "   ";
+            cout << matrix[i][j] << "   ";
         }
 
-        std::cout << std::endl;
+        cout << endl;
     }
 
-    std::cout << std::endl;
+    cout << endl;
 }
 
 int main()
 {
     int nrOfRows, nrOfColumns;
 
-    std::cout << "Enter the number of rows: ";
-    std::cin >> nrOfRows;
+    cout << "Enter the number of rows: ";
+    cin >> nrOfRows;
 
-    std::cout << "Enter the number of columns: ";
-    std::cin >> nrOfColumns; //TODO CASES WHEN MATRIX IS 1xn OR mx1
+    cout << "Enter the number of columns: ";
+    cin >> nrOfColumns; //TODO CASES WHEN MATRIX IS 1xn OR mx1
 
     int ans;
     bool toRREF;
 
-    std::cout << "Calculate to RREF (1) or to REF (2): ";
-    std:: cin >> ans;
+    cout << "Calculate to RREF (1) or to REF (2): ";
+    cin >> ans;
 
+    std::vector<vector<int>> matrixV;
     double **matrix = new double*[nrOfRows];
 
     for (int i = 0; i < nrOfRows; i++)
@@ -125,20 +132,20 @@ int main()
     {
         for (int j = 0; j < nrOfColumns; j++)
         {
-            std::cout << "Enter element " << j + 1 << " of row " << i + 1 << ": ";
-            std::cin >> matrix[i][j];
+            cout << "Enter element " << j + 1 << " of row " << i + 1 << ": ";
+            cin >> matrix[i][j];
         }
     }
 
     ref(matrix, nrOfRows, nrOfColumns);
 
-    std::cout << "The REF is: \n";
+    cout << "The REF is: \n";
     PrintMatrix(matrix, nrOfRows, nrOfColumns);
 
     if (toRREF)
     {
         rref(matrix, nrOfRows, nrOfColumns);
-        std::cout << "The RREF is: \n";
+        cout << "The RREF is: \n";
         PrintMatrix(matrix, nrOfRows, nrOfColumns);
     }
     
